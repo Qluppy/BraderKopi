ddaab5e2c67c1d236c991cdb84943f996d2d3cda
raihanaditya05 <raihanadit40@gmail.com>
2024-11-25 22:28:49 +0800

    memperbaiki database akun

--- database/migrations/2024_10_03_002903_create_akun_table.php
+++ database/migrations/2024_10_03_002903_create_akun_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('username')->unique();
             $table->string('password');
-            $table->enum('role', ['admin', 'kasir']);
+            $table->enum('role', ['kasir', 'admin']);
             $table->timestamps();
         });
     }
